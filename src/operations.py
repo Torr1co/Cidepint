1e45043ec1c387f4641dd0c53179f887e43b4f36
Ulises Geymonat <ulises.geymonat186925@alumnos.info.unlp.edu.ar>
2023-08-28 16:44:52 -0300

    moved operations from src/calculator.py to src/operations.py

---
+++ src/operations.py
@@ -0,0 +1,11 @@
+def divide (a,b):
+    if b!=0:
+        return a/b
+    else:
+        return "Error! Division by zero is not possible."
+
+def substraction (a,b):
+    return a-b
+  
+def add(a,b):
+    return a+b
