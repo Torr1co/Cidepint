8871d25fa0fa86c9e6282e8bcc833de91ced593a
ColoBonoris <nicolas.bonoris194136@alumnos.info.unlp.edu.ar>
2023-08-28 17:07:21 -0300

    Cleaned unused code

--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,3 @@
-def divide (a,b):
-    if b!=0:
-        return a/b
-    else:
-        return "Error! Division by zero is not possible."
 
-def add(a,b):
-    try:
-        return (a+b)
-    except TypeError:
-        print("Los argumentos ingresados deben ser numeros")
 
     
