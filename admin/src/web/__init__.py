cc34ed87e04aa64d49e50d6a627b789c59f87438
ColoBonoris <nicolas.bonoris194136@alumnos.info.unlp.edu.ar>
2023-09-05 08:58:05 -0300

    Fixed structure errors in class

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
-def create_app():
+def create_app(env="development", static_folder="./static"):
     """"Create app docstring"""
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=static_folder)
 
     @app.get("/")
     def home():
-        return "Hola munsdoaawsdasd"
+        return render_template("home.html")
     
     return app
