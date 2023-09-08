11ad10468700293a71cf324220bdc14bd69e597e
Austin <austin.myles192994@alumnos.info.unlp.edu.ar>
2023-09-07 21:05:28 -0300

    Added 404 error handler and made the static css folder

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -9,4 +9,9 @@ def create_app(env="development", static_folder="./static"):
     def home():
         return render_template("home.html")
     
+    # Controlador para manejar errores 404
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404
+    
     return app
