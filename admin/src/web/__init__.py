7e034986a56b403b132faffff1f809689eb2557d
ColoBonoris <nicolas.bonoris194136@alumnos.info.unlp.edu.ar>
2023-09-14 11:40:05 -0300

    New errors handling added, style changes made

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -9,21 +9,18 @@ def create_app(env="development", static_folder="../../static"):
     def home():
         return render_template("home.html")
     
-    # Controlador para manejar errores
+    # Controladores para manejar errores
     @app.errorhandler(401)
     def unauthorized(error):
-        return render_template("401.html"), 401
-    @app.errorhandler(402)
-    def payment_required(error):
-        return render_template("402.html"), 402
+        return render_template("errors/401.html"), 401
     @app.errorhandler(403)
     def forbidden(error):
-        return render_template("403.html"), 403
+        return render_template("errors/403.html"), 403
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template("404.html"), 404
+        return render_template("errors/404.html"), 404
     @app.errorhandler(500)
     def internal_server_error(error):
-        return render_template("500.html"), 500
+        return render_template("errors/500.html"), 500
     
     return app
