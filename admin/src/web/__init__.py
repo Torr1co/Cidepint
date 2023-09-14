9d5ee526388d76da6f55af2b94203cea41715bc7
ColoBonoris <nicolas.bonoris194136@alumnos.info.unlp.edu.ar>
2023-09-14 11:18:08 -0300

    Important structure changes and debug

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-def create_app(env="development", static_folder="./static"):
+def create_app(env="development", static_folder="../../static"):
     """"Create app docstring"""
     app = Flask(__name__, static_folder=static_folder)
 
@@ -9,9 +9,21 @@ def create_app(env="development", static_folder="./static"):
     def home():
         return render_template("home.html")
     
-    # Controlador para manejar errores 404
+    # Controlador para manejar errores
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return render_template("401.html"), 401
+    @app.errorhandler(402)
+    def payment_required(error):
+        return render_template("402.html"), 402
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template("403.html"), 403
     @app.errorhandler(404)
     def page_not_found(error):
         return render_template("404.html"), 404
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template("500.html"), 500
     
     return app
